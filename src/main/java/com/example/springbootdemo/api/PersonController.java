62129bcd3cd6ced98da532ad779bdae52b12eff9
Isaac Aderonmu <aderonmuisaac@gmail.com>
2021-01-04 13:20:02 +0100

    feat: GET request endpoint to return all people

--- src/main/java/com/example/springbootdemo/api/PersonController.java
+++ src/main/java/com/example/springbootdemo/api/PersonController.java
@@ -3,10 +3,9 @@ package com.example.springbootdemo.api;
 import com.example.springbootdemo.model.Person;
 import com.example.springbootdemo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("api/v1/person")
 @RestController
@@ -22,5 +21,10 @@ public class PersonController {
     @PostMapping
     public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople() {
+        return personService.getAllPeople();
     }
 }
