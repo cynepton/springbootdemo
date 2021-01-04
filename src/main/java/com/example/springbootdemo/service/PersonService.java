efb7c025831dc66c674ed4d8e2a0e8fd0c533333
Isaac Aderonmu <aderonmuisaac@gmail.com>
2021-01-04 12:42:56 +0100

    feat: Person Service class

---
+++ src/main/java/com/example/springbootdemo/service/PersonService.java
@@ -0,0 +1,22 @@
+package com.example.springbootdemo.service;
+
+import com.example.springbootdemo.dao.PersonDao;
+import com.example.springbootdemo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        return 1;
+    }
+}
