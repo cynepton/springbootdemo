29798ca9e20831f7e50ec14273369d0f9f794d57
Isaac Aderonmu <aderonmuisaac@gmail.com>
2021-01-04 12:42:01 +0100

    feat: Person model class

--- src/main/java/com/example/springbootdemo/model/Person.java
+++ src/main/java/com/example/springbootdemo/model/Person.java
@@ -1,21 +1,40 @@
 package com.example.springbootdemo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.UUID;
 
+/**
+ * This initiates the Person Model.
+ */
 public class Person {
-
+    // Class Attributes
     private final UUID id;
     private final String name;
 
-    public Person(UUID id, String name) {
+    /**
+     * Class Constructor for the Person Model class.
+     * @param id The Person ID
+     * @param name The Person Name
+     */
+    public Person(@JsonProperty("id") UUID id,
+                  @JsonProperty("name") String name) {
         this.id = id;
         this.name = name;
     }
 
+    /**
+     * Returns the Person ID
+     * @return The Person ID
+     */
     public UUID getId() {
         return id;
     }
 
+    /**
+     * Returns the Person Name
+     * @return The Person Name
+     */
     public String getName() {
         return name;
     }
