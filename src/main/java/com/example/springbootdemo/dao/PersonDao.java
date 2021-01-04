ebd56c712013836ee6564049d7bb389cfb78d2b3
Isaac Aderonmu <aderonmuisaac@gmail.com>
2021-01-04 13:18:06 +0100

    feat: select all people method

--- src/main/java/com/example/springbootdemo/dao/PersonDao.java
+++ src/main/java/com/example/springbootdemo/dao/PersonDao.java
@@ -2,6 +2,7 @@ package com.example.springbootdemo.dao;
 
 import com.example.springbootdemo.model.Person;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -20,4 +21,6 @@ public interface PersonDao {
         UUID id = UUID.randomUUID();
         return insertPerson(id, person);
     }
+
+    List<Person> selectAllPeople();
 }
