0ba27c43ebe9b26b5d93120db5c09a10073075ed
Isaac Aderonmu <aderonmuisaac@gmail.com>
2021-01-04 08:02:54 +0100

    feat: created fakePersonDataAccessService

---
+++ src/main/java/com/example/springbootdemo/dao/fakePersonDataAccessService.java
@@ -0,0 +1,18 @@
+package com.example.springbootdemo.dao;
+
+import com.example.springbootdemo.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class fakePersonDataAccessService implements PersonDao {
+
+    private static List<Person> DB = new ArrayList<>();
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        DB.add(new Person(id, person.getName()));
+        return 0;
+    }
+}
